1ac8366031245a8f5115a1b23741f38f3212070e
Prasun kr Mondal <prasunmondal660@gmail.com>
2024-02-02 10:08:44 +0530

    minor changes

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -6,6 +6,12 @@ function App() {
   let [Counter,  setCounter] =
   useState(0)
 
+  const multiplyByTwo = () => {
+   
+    setCounter(Counter * 2)
+
+  }
+
   const addValue = () => {
    
     setCounter(Counter + 1)
@@ -22,19 +28,23 @@ function App() {
   }
 
   return (
-    <>
+    <div className="app-container">
     <h1>Counter Value {Counter}</h1>
   
-
+    <div className="button-container">
     <button 
     onClick={addValue} disabled={Counter === 10}>Add Value </button>
     <br />
     <button
     onClick={removeValue} disabled={Counter === 0}>Remove Value </button>
+    <button 
+    onClick={multiplyByTwo}>x 2</button>
 
     <button 
     onClick={resetValue}>Reset</button>
-    </>
+    </div>
+    
+    </div>
   )
 }
 
